40b0bb819105f8a2125e3a13f4bfb4393340b70d
Anuvind P <anuvind.p@cabotsolutions.onmicrosoft.com>
2023-07-28 19:54:31 +0530

    dashboard controllers added

---
+++ Dto/Response/GetDoctorDashboardInformation.cs
@@ -0,0 +1,24 @@
+﻿namespace WebAPI_Wa.Dto.Response
+{
+    public class GetDoctorDashboardInformation
+    {
+        public TotalConsultedCases totalConsultedCases { get; set; } = new TotalConsultedCases();   
+        public OnGoingConsultation onGoingConsultation { get; set; } = new OnGoingConsultation();
+    }
+    public class TotalConsultedCases
+    {
+        public int IpCount { get; set; }
+        public int OpPort { get; set; }
+    }
+    public class OnGoingConsultation
+    {
+        public int IpCount { get; set; }
+        public int OpPort { get; set; }
+        public CriticalPersonPercentage criticalPersonPercentage { get; set; } = new CriticalPersonPercentage();
+    }
+    public class CriticalPersonPercentage
+    {
+        public int IpCount { get; set; }
+        public int OpPort { get; set; }
+    }
+}
---
+++ Dto/Response/GetHospitalCountRespone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace WebAPI_Wa.Dto.Response
+{
+    public class GetHospitalCountRespone
+    {
+        public List<HospitalsDto> Hospitals { get; set; }
+    }
+    public class HospitalsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
